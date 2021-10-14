c607c1266021c9c3b920b6adea886c09afe1276d
Anantharao1998 <anantharao1998@gmail.com>
2021-10-14 17:49:13 +0800

    feat: created working chat app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import socketIOClient from "socket.io-client";
+import React from "react";
 
 function App() {
+  const socket = socketIOClient("http://192.168.100.188:5000");
+  let [name, setName] = React.useState("");
+  let [message, setMessage] = React.useState("");
+  const [message_list, setMessageList] = React.useState([]);
+
+  const sendMessage = () => {
+    socket.emit("chat", {
+      username: name,
+      message: message,
+    });
+  };
+
+  React.useEffect(() => {
+    socket.on("chat", function (data) {
+      setMessageList((current) => [...current, data]);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>BeSquare Chatroom</h1>
       </header>
+      <body>
+        <div className="container">
+          <div className="chats">
+            {message_list.map((msg, index) => {
+              return (
+                <p>
+                  {msg.username} : {msg.message}
+                </p>
+              );
+            })}
+          </div>
+        </div>
+        <p id="label">Name:</p>
+        <input onChange={(event) => setName(event.target.value)}></input>
+        <p id="label">Message:</p>
+        <input onChange={(event) => setMessage(event.target.value)}></input>
+        <button onClick={sendMessage}>Send</button>
+      </body>
     </div>
   );
 }
